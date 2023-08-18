3e2ec6bfb3e0a7e60206484591d73227ef012ac1
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2023-08-18 17:30:24 +0700

    add loading page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,34 @@
 "use client";
-import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import About from "../components/About";
 import Footer from "../components/Footer";
 import BallAnimation from "../components/BallAnimation";
+import LoaderPage from "../components/LoaderPage";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <main className="flex  flex-col items-center  ">
-      <div className="fixed w-screen h-screen bg-violet-300/10 -z-50" />
-      <div className="w-full space-y-6">
-        <Navbar />
-        <Hero />
-        <BallAnimation/>
-        <div className="p-12" />
-        <Footer />
-      </div>
-    </main>
+    <>
+      <AnimatePresence>{loading && <LoaderPage />}</AnimatePresence>
+      <main className="flex  flex-col items-center  ">
+        <div className="fixed w-screen h-screen bg-violet-300/10 -z-50" />
+        <div className="w-full space-y-6">
+          <Navbar />
+          <Hero />
+          <BallAnimation />
+          <div className="p-12" />
+          <Footer />
+        </div>
+      </main>
+    </>
   );
 }
---
+++ components/LoaderPage.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Lottie from "lottie-react";
+import animationData from "../public/animLoader/data.json";
+import Shine from "./Shine";
+import { motion as m, AnimatePresence } from "framer-motion";
+
+export default function LoaderPage() {
+  return (
+    <>
+      <m.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="text-white h-screen w-screen bg-black fixed z-[999900]"
+      >
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="w-64 h-64">
+            <Lottie animationData={animationData} loop={false} />
+          </div>
+        </div>
+
+        <Shine />
+      </m.div>
+    </>
+  );
+}
