c5be45f5a0e6d5786d2e1897726ca5ad8601936f
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2023-08-18 15:53:03 +0700

    finish main animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,6 +13,9 @@ const navigation = [
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+  const zoraLink = 'https://zora.co/collect/base:0xbad640dd4472fd98cbdc1b42a3f0663d901ba345'
+  const mintFunLink = 'https://mint.fun/base/0xbaD640dd4472Fd98cbDC1b42a3f0663d901Ba345'
+
   return (
     <>
       <div className="w-full justify-center flex flex-col items-center pt-[10vh]">
